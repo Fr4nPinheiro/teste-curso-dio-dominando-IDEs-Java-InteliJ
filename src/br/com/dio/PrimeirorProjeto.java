2d26f232c177ce5408ab85a36944897938053f01
Francielle Pinheiro <fr4npinheiro@hotmail.com>
2022-07-16 08:30:55 -0300

    criando class livro

--- src/br/com/dio/PrimeirorProjeto.java
+++ src/br/com/dio/PrimeirorProjeto.java
@@ -9,10 +9,48 @@ public class PrimeirorProjeto {
         System.out.println(gato);
 
 
+        Livro livro1 = new Livro("o problema dos 3 corpos ", 300);
+
+        System.out.println(livro1);
+
+
         /*int a = 3;
         int b = 2;
         System.out.println("Hello World! " + (a+b));*/
     }
 }
 
+class Livro {
+    private String nome;
+    private Integer npag;
+
+    public Livro(String nome, Integer npag) {
+        this.nome = nome;
+        this.npag = npag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpag() {
+        return npag;
+    }
+
+    public void setNpag(Integer npag) {
+        this.npag = npag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", npag=" + npag +
+                '}';
+    }
+}
 
